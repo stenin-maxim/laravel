0dc1ab021a61fe088eb56da4d60bf94e447aeafa
Maxim <stenin.dev@gmail.com>
2024-08-07 10:43:41 +0300

    Add routes

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
-Route::get('/roles', [RoleController::class, 'roles']);
+Route::get('/users', [UserController::class, 'index']);
+Route::get('/user/{id}', [UserController::class, 'show']);
+
+Route::get('/roles', [RoleController::class, 'index']);
+Route::get('/role/{id}', [RoleController::class, 'show']);
